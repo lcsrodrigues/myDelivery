8c02ab9e127a9050024484e87ba57bbe7988ced7
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2021-05-06 02:28:01 -0300

    Adição da tela de login e rotas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
-import { Header, Body, Footer, Wizard } from '../public/components';
+import styles from '../public/css/login.module.scss';
+import { Header, Footer } from '../public/components'
+import { BtnLogar } from '../public/components/buttons/buttons';
 
-const Home = () => {
+const Login = () => {
     return (
-        <>
+        <div className={styles.login}>
             <Header />
-            <Wizard />
-            <Body />
+            <section>
+                <div className={styles.sideLeft}>
+                    <div className={styles.backgroundImage}></div>
+                </div>
+                <div className={styles.sideRight}>
+                    <div className={styles.boxLogin}>
+                        <div className={styles.boxLoginHeader}>
+                            <span>LOGIN</span>
+                        </div>
+                        <div className={styles.boxLoginContent}>
+                            <input type={"text"} placeholder={"Email"} />
+                            <input type={"password"} placeholder={"Senha"} />
+                        </div>
+                        <div className={styles.boxLoginActions}>
+                            <BtnLogar />
+                            <p>
+                                Não tem conta? <strong>Cadastre-se</strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <Footer />
-        </>
+        </div>
     )
 }
 
-export default Home;
+export default Login;
